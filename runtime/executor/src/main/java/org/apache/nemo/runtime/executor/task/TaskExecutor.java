e2184672dcd04ceda22915c5274b952bc03c0acb
taegeonum <taegeonum@gmail.com>
2019-03-15 10:33:10 +0900

    log

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/task/TaskExecutor.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/task/TaskExecutor.java
@@ -662,8 +662,7 @@ public final class TaskExecutor {
 
         if (elem instanceof Watermark) {
           final Watermark watermark = (Watermark) elem;
-          //LOG.info("Receive watermark {}", watermark);
-
+          LOG.info("Receive watermark {} for {}", watermark, interOp.getNextOperator().getId());
           interOp.getWatermarkManager().trackAndEmitWatermarks(interOp.getEdgeIndex(), watermark);
 
         } else if (elem instanceof TimestampAndValue) {
