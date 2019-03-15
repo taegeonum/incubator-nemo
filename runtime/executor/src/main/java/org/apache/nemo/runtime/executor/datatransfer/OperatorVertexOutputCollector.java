78d80a39c05ad7f8e1fec918de7f8d20f2782f3c
taegeonum <taegeonum@gmail.com>
2019-03-15 09:51:37 +0900

    logging

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/OperatorVertexOutputCollector.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/OperatorVertexOutputCollector.java
@@ -246,7 +246,7 @@ public final class OperatorVertexOutputCollector<O> extends AbstractOutputCollec
       } else {
         final Pair<OperatorMetricCollector, OutputCollector> pair =
           outputCollectorMap.get(internalVertex.getNextOperator().getId());
-        //LOG.info("Internal Watermark {} emit to {}", watermark, internalVertex.getNextOperator().getId());
+        LOG.info("Internal Watermark {} emit to {}", watermark, internalVertex.getNextOperator().getId());
         internalVertex.getWatermarkManager().trackAndEmitWatermarks(internalVertex.getEdgeIndex(), watermark);
       }
     }
@@ -261,7 +261,7 @@ public final class OperatorVertexOutputCollector<O> extends AbstractOutputCollec
         } else {
           final Pair<OperatorMetricCollector, OutputCollector> pair =
             outputCollectorMap.get(internalVertex.getNextOperator().getId());
-          //LOG.info("Internal Watermark {} emit to {}", watermark, internalVertex.getNextOperator().getId());
+          LOG.info("Internal Watermark {} emit to {}", watermark, internalVertex.getNextOperator().getId());
           internalVertex.getWatermarkManager().trackAndEmitWatermarks(internalVertex.getEdgeIndex(), watermark);
         }
       }
