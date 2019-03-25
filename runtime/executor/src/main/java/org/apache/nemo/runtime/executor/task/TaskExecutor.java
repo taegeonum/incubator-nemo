411610f9f6dbb187efb7c1d4f308c09165403f44
taegeonum <taegeonum@gmail.com>
2019-03-25 21:22:25 +0900

    logging

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/task/TaskExecutor.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/task/TaskExecutor.java
@@ -808,11 +808,15 @@ public final class TaskExecutor {
 
           } else if (data instanceof EndOffloadingKafkaEvent) {
 
+            LOG.info("Start -- Receive end offloading event {}", taskId);
             kafkaOffloader.get().handleEndOffloadingKafkaEvent();
+            LOG.info("End -- Receive end offloading event {}", taskId);
 
           } else if (data instanceof StartOffloadingKafkaEvent) {
 
+            LOG.info("Start -- handle start offloading kafka event {}", taskId);
             kafkaOffloader.get().handleStartOffloadingKafkaEvent();
+            LOG.info("End -- handle start offloading kafka event {}", taskId);
           } else {
             throw new RuntimeException("Unsupported type: " + data);
           }
