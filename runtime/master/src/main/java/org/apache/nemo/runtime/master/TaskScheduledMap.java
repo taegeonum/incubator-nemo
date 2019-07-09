7184936bf548a78ae646bd5dbacb8ef0e86513f1
taegeonum <taegeonum@gmail.com>
2019-07-09 15:27:21 +0900

    implement master scaling decision

---
+++ runtime/master/src/main/java/org/apache/nemo/runtime/master/TaskScheduledMap.java
@@ -0,0 +1,43 @@
+package org.apache.nemo.runtime.master;
+
+import org.apache.nemo.runtime.common.RuntimeIdManager;
+import org.apache.nemo.runtime.common.plan.Task;
+import org.apache.nemo.runtime.master.resource.ExecutorRepresenter;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public final class TaskScheduledMap {
+
+  private final ConcurrentMap<ExecutorRepresenter,
+    Map<String, List<Task>>> scheduledStageTasks;
+
+  @Inject
+  private TaskScheduledMap() {
+    this.scheduledStageTasks = new ConcurrentHashMap<>();
+  }
+
+  public void addTask(final ExecutorRepresenter representer, final Task task) {
+    scheduledStageTasks.putIfAbsent(representer, new HashMap<>());
+    final Map<String, List<Task>> stageTaskMap = scheduledStageTasks.get(representer);
+
+    synchronized (stageTaskMap) {
+      final String stageId = RuntimeIdManager.getStageIdFromTaskId(task.getTaskId());
+      final List<Task> stageTasks = stageTaskMap.get(stageId);
+      stageTasks.add(task);
+    }
+  }
+
+  public ConcurrentMap<ExecutorRepresenter, Map<String, List<Task>>> getScheduledStageTasks() {
+    return scheduledStageTasks;
+  }
+
+  public Map<String, List<Task>> getScheduledStageTasks(final ExecutorRepresenter representer) {
+    return scheduledStageTasks.get(representer);
+  }
+}
