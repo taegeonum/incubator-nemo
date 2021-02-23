59cb6a0169f7d241247cca405583f4a49e353bc8
taegeonum <taegeonum@gmail.com>
2021-02-23 23:12:03 +0900

    default value

--- common/src/main/java/org/apache/nemo/common/parameters/FlushPeriod.java
+++ common/src/main/java/org/apache/nemo/common/parameters/FlushPeriod.java
@@ -3,6 +3,6 @@ package org.apache.nemo.common.parameters;
 import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.annotations.NamedParameter;
 
-@NamedParameter(doc = "flush period (ms)")
+@NamedParameter(doc = "flush period (ms)", default_value = "200")
 public final class FlushPeriod implements Name<Integer> {
 }
