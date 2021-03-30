2fbecda1e19d3af16fa2cb87b03bfe64f3070c23
taegeonum <taegeonum@gmail.com>
2021-03-30 21:59:47 +0900

    no sleep for stop task

--- runtime/master/src/main/java/org/apache/nemo/runtime/master/JobScaler.java
+++ runtime/master/src/main/java/org/apache/nemo/runtime/master/JobScaler.java
@@ -1292,7 +1292,7 @@ public final class JobScaler {
       LOG.info("Waiting for task rescheduling {}", taskId);
       while (!taskScheduledMap.isTaskScheduled(taskId)) {
         try {
-          Thread.sleep(50);
+          Thread.sleep(10);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
