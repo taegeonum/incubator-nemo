4829702414f1e2d142decfe21abaa399986ff163
taegeonum <taegeonum@gmail.com>
2021-04-01 22:14:20 +0900

    sleep 100ms

--- runtime/driver/src/main/java/org/apache/nemo/driver/NemoDriver.java
+++ runtime/driver/src/main/java/org/apache/nemo/driver/NemoDriver.java
@@ -235,6 +235,7 @@ public final class NemoDriver {
                 .collect(Collectors.toList());
             for (int i = stages.size() - 1; i >= 0; i--) {
               jobScaler.sendTaskStopSignal(num, Collections.singletonList(stages.get(i)));
+              Thread.sleep(100);
             }
             // runtimeMaster.triggerConditionalRouting(true, evalConf.partialPercent * 0.01);
           } else if (decision.equals("reclaim-task")) {
